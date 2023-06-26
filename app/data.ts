3dd990635afbfc98b66242db3be2a99fa96ef2c6
waleedd32 <valid0@hotmail.com>
2023-06-26 16:59:33 +0300

    Added data.ts with quiz structure and placeholders

---
+++ app/data.ts
@@ -0,0 +1,35 @@
+export const quiz = {
+  totalQuestions: 5,
+  questions: [
+    {
+      id: 1,
+      question: "",
+      answers: ["", "", "", ""],
+      correctAnswer: "",
+    },
+    {
+      id: 2,
+      question: "",
+      answers: ["", "", "", ""],
+      correctAnswer: "",
+    },
+    {
+      id: 3,
+      question: "",
+      answers: ["", "", "", ""],
+      correctAnswer: "",
+    },
+    {
+      id: 4,
+      question: "",
+      answers: ["", "", "", ""],
+      correctAnswer: "",
+    },
+    {
+      id: 5,
+      question: "",
+      answers: ["", "", "", ""],
+      correctAnswer: "",
+    },
+  ],
+};
