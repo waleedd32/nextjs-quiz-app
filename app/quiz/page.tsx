37b14eee3f3c56a574e5cdb971b1b650840e8e3c
waleedd32 <valid0@hotmail.com>
2023-06-28 01:38:19 +0300

    Added structure to Quiz Page

--- app/quiz/page.tsx
+++ app/quiz/page.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 function page() {
-  return <div>Quiz Page</div>;
+  return (
+    <div className="container">
+      <h1>Quiz Page</h1>
+      <div>
+        <h2>
+          Question: 1<span></span>
+        </h2>
+      </div>
+    </div>
+  );
 }
 
 export default page;
