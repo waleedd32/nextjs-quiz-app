ee23a2c6f90bdc497ebbfb427941aa964491ac31
waleedd32 <valid0@hotmail.com>
2023-07-03 19:30:17 +0300

    Refactor Home component for flexibility and testability

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 import Link from "next/link";
 
-export default function Home() {
+const Home = ({ quizUrl = "/quiz" }) => {
   return (
     <main>
       <div className="container">
         <h1>Quiz App</h1>
-        <Link href="/quiz">
+        {/* <Link href="/quiz"> */}
+        <Link href={quizUrl}>
           <button>Start Quiz</button>
         </Link>
       </div>
     </main>
   );
-}
+};
+export default Home;
